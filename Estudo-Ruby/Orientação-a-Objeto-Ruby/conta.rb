71a01be46d1115a3eb8a2a805e5b363203d743fe
LeOzInNDF <leo_albano@hotmail.com>
2024-02-29 23:54:35 -0300

    update-code

---
+++ Estudo-Ruby/Orientação-a-Objeto-Ruby/conta.rb
@@ -0,0 +1,27 @@
+class Conta 
+  attr_reader :numero, :titular
+  attr_accessor :saldo
+
+  def initialize(numero, titular, saldo)
+   @numero = numero
+   @titular = titular
+   @saldo = saldo
+  end
+
+  def sacar(valor)
+    if saldo >= valor
+      self.saldo -= valor
+    else
+      puts "Não foi possivel executar o saque"
+    end
+  end
+
+  def depositar(valor)
+    self.saldo += valor
+end
+
+  def tranferir(conta_destino, valor)
+    sacar(valor)
+
+  end
+end
--- conta.rb
+++
@@ -1,7 +0,0 @@
-class conta 
-  def initialize(numero, titular, saldo)
-   @numero = numero
-   @titular = titular
-   @saldo = saldo
-  end
-end
